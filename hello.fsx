30a8168ea1a62f0a3cbe82d04c289dc773523546
JacobMarineau <jacobmarineaump@gmail.com>
2024-10-30 16:29:47 -0500

    Learning about more

--- hello.fsx
+++ hello.fsx
@@ -1,1 +1,11 @@
 printfn "Hello World from F#"
+
+let printMessage name =
+    printfn $"Hello there, {name}!"
+
+let printNames names =
+    for name in names do
+        printMessage name
+
+let names = [ "Ana"; "Felipe"; "Emillia" ]
+printNames names
